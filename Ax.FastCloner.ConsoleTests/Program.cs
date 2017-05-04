f3353d135d20874bc3cf882785766a4ee6620b38
adrianaxente <adrianaxente@yahoo.com>
2017-05-05 00:56:03 +0300

    - Added Console Test Project

---
+++ Ax.FastCloner.ConsoleTests/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ax.FastCloner.ConsoleTests
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
